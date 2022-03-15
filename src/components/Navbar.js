d7993d64bca0ac225a3254944288ccd789f07add
Pablo Vittar <vitpablo1@gmail.com>
2022-03-15 10:23:02 -0300

    limpieza

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,33 +63,24 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
+              <Link className="navbar-item" to="/">
+                home
+              </Link>
               <Link className="navbar-item" to="/about">
                 About
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                graphic mock ups
               </Link>
               <Link className="navbar-item" to="/blog">
-                Blog
+                product mock ups
               </Link>
-              <Link className="navbar-item" to="/contact">
+              <Link className="navbar-item" to="/">
                 Contact
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
+              <Link className="navbar-item" to="/">
+                home{" "}
               </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
             </div>
           </div>
         </div>
