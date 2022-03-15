b6acc8e50321e3ec4b5d436168d4943951159a93
Pablo Vittar <vitpablo1@gmail.com>
2022-03-15 13:58:25 -0300

    cleanup

---
+++ src/pages/about/index.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+import Link from "gatsby-link";
+import Layout from "../../components/Layout";
+
+export default class Index extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <section>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia,
+          veritatis! Qui deleniti maxime unde tempore molestiae autem labore
+          commodi, voluptas, corrupti quos velit. Unde est aliquid odit?
+          Consequatur, ipsam itaque! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Delectus eligendi et id perspiciatis repellat quidem
+          cumque libero consequuntur molestias dolores commodi, vel
+          exercitationem non odit fugiat magni saepe? Reprehenderit, aut.
+        </section>
+      </Layout>
+    );
+  }
+}
